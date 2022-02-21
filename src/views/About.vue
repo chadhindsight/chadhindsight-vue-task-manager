52b2a8141a35a92ad7f0b7c0a14aca42956b226b
chad hinds <chadrickj8@gmail.com>
2022-02-21 17:43:47 -0500

    add umbrella comment

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
   <h3>Version 1.0.0</h3>
   <router-link to="/">Go back</router-link>
 </template>
-
 <script>
+// umbrella
 export default {
   name: "About",
   inheritAttrs: false, // disable 'non-props' warning
